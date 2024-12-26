51a858ca8f279160b1aa957594f408a650962612
seymagnn <seymagunonu@gmail.com>
2024-12-26 20:10:52 +0300

    .

--- WeatherApp/Components/WeatherRow.swift
+++ WeatherApp/Components/WeatherRow.swift
@@ -8,11 +8,32 @@
 import SwiftUI
 
 struct WeatherRow: View {
+    var logo : String
+    var name : String
+    var value : String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing: 20){
+            Image(systemName: logo)
+                .font(.title2)
+                .frame(width: 20,height: 20)
+                .padding()
+                .background(.gray.opacity(0.4))
+                .cornerRadius(50)
+            
+            VStack(alignment: .leading, spacing: 8){
+                Text(name)
+                    .font(.caption)
+                
+                Text(value)
+                    .bold()
+                    .font(.title)
+            }
+        }
+        
     }
 }
 
 #Preview {
-    WeatherRow()
+    WeatherRow(logo: "thermometer", name: "Feels Like", value: "6°")
 }
--- WeatherApp/Views/WeatherView.swift
+++ WeatherApp/Views/WeatherView.swift
@@ -8,11 +8,93 @@
 import SwiftUI
 
 struct WeatherView: View {
+    var weather: ResponseBody
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(alignment: .leading) {
+            VStack {
+                VStack(alignment: .leading, spacing: 5) {
+                    Text(weather.name)
+                        .bold()
+                        .font(.title)
+                    
+                    Text("Today, \(Date().formatted(.dateTime.month().day().hour().minute()))")
+                        .fontWeight(.light)
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                
+                Spacer()
+                
+                VStack {
+                    HStack {
+                        VStack(spacing: 20) {
+                            Image(systemName: "cloud")
+                                .font(.system(size: 40))
+                            
+                            Text(weather.weather[0].main)
+                        }
+                        .frame(maxWidth: 150, alignment: .leading)
+                        
+                        Spacer()
+                        
+                        Text(weather.main.feels_like.roundDouble() + "°")
+                            .font(.system(size: 100))
+                            .fontWeight(.bold)
+                            .padding()
+                    }
+                    
+                    Spacer()
+                        .frame(height: 80)
+                    
+                    AsyncImage(url: URL(string: "https://www.pocketful.in/blog/wp-content/smush-webp/2024/03/Building-bro-1536x1536.png.webp")) { image in
+                        image
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 350)
+                    } placeholder: {
+                        ProgressView()
+                    }
+                    
+                    Spacer()
+                        
+                }
+                .frame(maxWidth: .infinity)
+            }
+            .padding()
+            .frame(maxWidth: .infinity, alignment: .leading)
+            
+            VStack {
+                Spacer()
+                
+                VStack(alignment: .leading, spacing: 20) {
+                    Text("Weather now")
+                        .bold().padding(.bottom)
+                    
+                    HStack {
+                        WeatherRow(logo: "thermometer", name: "Min temp", value: (weather.main.tempMin.roundDouble() + "°"))
+                        Spacer()
+                        WeatherRow(logo: "thermometer", name: "Max temp", value: (weather.main.tempMax.roundDouble() + "°"))
+                    }
+                    HStack {
+                        WeatherRow(logo: "wind", name: "Wind speed", value: (weather.wind.speed.roundDouble() + "m/s"))
+                        Spacer()
+                        WeatherRow(logo: "humidity", name: "Humidity", value: (weather.main.humidity.roundDouble() + "%"))
+                    }
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding()
+                .padding(.bottom, 20)
+                .foregroundColor(Color(hue: 0.656, saturation: 0.787, brightness: 0.354))
+                .background(.white)
+                .cornerRadius(20, corners: [.topLeft, .topRight])
+            }
+        }
+        .edgesIgnoringSafeArea(.bottom)
+        .background(LinearGradient(colors: [.blue, .white], startPoint: .leading, endPoint: .bottom))
+        .preferredColorScheme(.dark)
     }
 }
 
 #Preview {
-    WeatherView()
+    WeatherView(weather: previewWeather)
 }
